8785f69f44902ed317fbbd3b90c3e1bb4d60e469
yexudong <ye850570138@sina.com>
2018-12-20 17:57:36 +0800

    Initial commit

---
+++ app/src/main/java/com/tinkerdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tinkerdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * https://www.cnblogs.com/huangjialin/p/7693816.html
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
